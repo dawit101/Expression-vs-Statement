f9aa14eec8e9a1bfeae25aaabd3b1e2f2d876f04
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 05:52:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Expressions vs Statement
+#Expression = a piece a code that produces value
+#Statement = Entire line of code that performs a action
+
+iq = 100 #statement 
+user_age = iq/4 # iq/4 is expression and the whole line of code is a statement
+
+print(int(user_age))
